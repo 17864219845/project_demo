cf94d67e3b702b2020fa456a7795b1716b0dac63
zhouxiaowei01 <zhouxiaowei01@qianxin.com>
2025-06-24 16:13:29 +0800

    init 6

--- api/test.py
+++ api/test.py
@@ -5,13 +5,15 @@ import uuid
 
 import requests
 
-res = dir(builtins)
+from configs import config
+
+res111 = dir(builtins)
 # print(res)
 
 # result = requests.get("http://10.43.160.198:5000/internal/api/home")
 # if result.status_code != 200:
 #     print('no', result.json())
 
-a = uuid.uuid4().hex
-
+res = config
+a=res.DEBUG
 print(a)
