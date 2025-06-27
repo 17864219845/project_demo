a1a5f0fc9b0eef65b2da68a2ffd2c0a6a7848f98
zhouxiaowei01 <zhouxiaowei01@qianxin.com>
2025-06-27 11:11:46 +0800

    andi5

--- api/test.py
+++ api/test.py
@@ -1,22 +1,8 @@
 
-
-import builtins
-import uuid
-
-import requests
-
 from configs import config
+print(config)
 
 # res111 = dir(builtins)
 # print(res)
-
-# result = requests.get("http://10.43.160.198:5000/internal/api/home")
-# if result.status_code != 200:
-#     print('no', result.json())
-
-
-import flask_migrate
-
-flask_migrate.upgrade()
 
 
